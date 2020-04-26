18601008494892d25cf66692cbba93658f51850c
thanhtung3496 <33385100+thanhtung3496@users.noreply.github.com>
2020-04-26 16:34:21 -0400

    Added Like button using AJAX

--- src/likeBtnClick.php
+++ src/likeBtnClick.php
@@ -1,10 +1,13 @@
 <?php
 require_once('../includes/database.php');
+require_once('../includes/authentication.php');
 
-//if (isset($_REQUEST['id']) && $_REQUEST['id'] >= 0)
+if (is_logged() && isset($_REQUEST['id']) && $_REQUEST['id'] >= 0) {
 	$id = $_REQUEST['id'];
     $likeCount = $database->incrementLike($id);
     echo "( " . $likeCount . " Likes)";
-//}
+} else {
+    echo "N/A";
+}
 
 ?>
